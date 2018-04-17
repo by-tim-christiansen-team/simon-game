a036733b6f6fdbb02532fafa9cf66b66e1f252e2
Hauke Grothues <hauke.grothues@web.de>
2018-04-17 18:23:44 +0200

    Integrating JS

--- js/script.js
+++ js/script.js
@@ -12,14 +12,14 @@ $(document).ready(function() {
   console.log("Program running.");
 
   // toggle strict mode
-  $(".strict-btn").click(function() {
+  $("#strict").click(function() {
     gameData.strictMode = !gameData.strictMode;
     $(".strict-led").toggleClass("led-active");
     console.log(gameData);
   });
 
   // setup and start new game on button-click
-  $(".start-game").click(function() {
+  $("#btn").click(function() {
     $(this).text("RESET");
     gameData.currentPattern = [];
     gameData.score = 17;
@@ -29,7 +29,7 @@ $(document).ready(function() {
 
   // add one color to pattern
   function setupNewRound() {
-    $(".buttons-circle").addClass("disable-clicks");      // disable buttons
+    $("#circle").addClass("disable-clicks");      // disable buttons
     $(".score-value").text(gameData.score);             // display new score
     var newColor;
     var num = Math.floor(Math.random() * (4 - 1 + 1)) + 1;  // generate random number between 1 and 4
@@ -61,35 +61,35 @@ $(document).ready(function() {
     var moves = setInterval(function() {
       var field = gameData.currentPattern[i];
       new Audio("sounds/" + gameData.currentPattern[i] + "Sound.mp3").play();
-      $("." + field).addClass("active");
+      $("#" + field).addClass("active");
       setTimeout(function() {
-        $("." + field).removeClass("active");
-      }, 600);
+        $("#" + field).removeClass("active");
+      }, 400);
       i++;
       if (i >= gameData.currentPattern.length) {
         clearInterval(moves);
-        $(".buttons-circle").removeClass("disable-clicks");     // enable buttons
+        $("#circle").removeClass("disable-clicks");     // enable buttons
       }
-    }, 1000);
+    }, 700);
   }
 
     // play sound effect and push clicked color to usersPattern array
   function clickFunction() {
-    $(".quarter-btn").click(function() {
+    $(".quarter").click(function() {
       switch($(this).css("background-color")) {
-        case "rgb(255, 0, 0)":
+        case "rgb(247, 57, 85)":
           new Audio("sounds/redSound.mp3").play();
           gameData.usersPattern.push("red");
           break;
-        case "rgb(0, 128, 0)":
+        case "rgb(69, 176, 156)":
           new Audio("sounds/greenSound.mp3").play();
           gameData.usersPattern.push("green");
           break;
-        case "rgb(0, 0, 255)":
+        case "rgb(42, 150, 189)":
           new Audio("sounds/blueSound.mp3").play();
           gameData.usersPattern.push("blue");
           break;
-        case "rgb(255, 255, 0)":
+        case "rgb(239, 201, 76)":
           new Audio("sounds/yellowSound.mp3").play();
           gameData.usersPattern.push("yellow");
           break;
@@ -107,16 +107,15 @@ $(document).ready(function() {
       console.log(gameData.currentPattern + " | " + gameData.usersPattern);
       if (gameData.usersPattern.length == gameData.currentPattern.length) {             // if the whole pattern was returned correctly
         console.log("Correct input.");
-        $(".buttons-circle").addClass("disable-clicks");          // disable buttons
+        $("#circle").addClass("disable-clicks");          // disable buttons
         gameData.score += 1;                                  // increase score by 1
         if (gameData.score == 20) {
           console.log("Game won!");
           $(".score-value").text(gameData.score);
-          showText($(".won"));
           gameData.currentPattern = [];
           gameData.score = 0;
         }
-        showText($(".correct"));
+        lightLamp($("#right"));
         setTimeout(function() {
           setupNewRound();
         }, 1000);
@@ -125,19 +124,19 @@ $(document).ready(function() {
     else {                                                // if input was wrong
       if(gameData.strictMode) {                           // strictMode activated; end game
         console.log("Game lost.");
-        showText($(".lost"));
+        lightLamp($("#fail"));
       }
       else {
-        showText($(".wrong"));
+        lightLamp($("#fail"));
         displayPattern();
       }
-      $(".buttons-circle").addClass("disable-clicks");      // disable buttons
+      $("#circle").addClass("disable-clicks");      // disable buttons
     }
   }
 
-  function showText(text) {
-    text.fadeIn("slow");
-    text.delay(500).fadeOut("slow");
+  function lightLamp(el) {
+    el.fadeIn("slow");
+    el.delay(500).fadeOut("slow");
   }
 
 });
